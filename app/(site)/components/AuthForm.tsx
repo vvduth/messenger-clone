127a415dfad7773304a03776b9d216736138aa26
gja5899 <anh-duc.thai@teliacompany.com>
2023-06-14 13:45:02 +0300

    placeholder for the auth UI done

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,10 @@
 "use client";
-import Input from "@/app/components/Input";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
 import React, { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -47,17 +50,79 @@ const AuthForm = () => {
 
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className="
+      <div
+        className="
       bg-whit px-4 py-8 shadow-sm sm:rounded-lg sm:px-10
       
-      ">
-        <form
-            className="space-w-6"
-            onSubmit={handleSubmit(onSubmit)}
-        >
-            <Input errors={errors} label="Email"  id="email" register={register} /> 
-
+      "
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              errors={errors}
+              label="Name"
+              id="name"
+              register={register}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            errors={errors}
+            label="Email"
+            type="email"
+            id="email"
+            disabled={isLoading}
+            register={register}
+          />
+          <Input
+            errors={errors}
+            label="Password"
+            type="password"
+            id="password"
+            disabled={isLoading}
+            register={register}
+          />
+          <Button
+            onClick={() => {}}
+            disabled={isLoading}
+            fullWidth
+            type="submit"
+          >
+            {variant === "LOGIN" ? "Sign in" : "Register"}{" "}
+          </Button>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton 
+              icon={BsGithub} 
+              onClick={() => socialAction('github')} 
+            />
+            <AuthSocialButton 
+              icon={BsGoogle} 
+              onClick={() => socialAction('google')} 
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant === "LOGIN"
+              ? "New to this app?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
